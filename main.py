6d670a5e9df245af9a3283f0c228adda8bf8e6f3
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-03-29 10:37:10 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+app.get("/")
+
+
+async def show_login_form():
+    return "login form"
+
+
+app.post("login")
+
+
+async def login():
+    return {"login"}
