c489c45d3b0bcfc069b13be1ed16628d0ce1d6a7
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-03-29 15:48:28 +0500

    model.py

--- model.py
+++ model.py
@@ -1,2 +1,9 @@
 from sqlalchemy import Column, DateTime, Integer, String
 from db import Base
+class enrty(Base):
+    __tablename__ = "sinup"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+    full_name = Column(String, index=True)
