586132dbc1e616bb319e8d091992bbb6de09607b
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-03-29 15:52:57 +0500

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+# Route to display the login form
 @app.get("/", response_class=HTMLResponse)
 async def show_login_form(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
 
-
+#use to encrypt the pawd
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
