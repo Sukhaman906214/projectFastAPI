d9562891948edf475192e3e0e1dbb9217317e1d7
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-03-30 15:55:30 +0500

    db.py

--- db.py
+++ db.py
@@ -1,1 +1,5 @@
+from sqlalchemy import create_engine
 
+DATABASE_URL = "sqlite:///./lg.db"
+# Create the database engine
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
