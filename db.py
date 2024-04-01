16e672e87937c470400e37f5e7b377d089387a43
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-04-01 09:55:55 +0500

    db.py

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 DATABASE_URL = "sqlite:///./lg.db"
 # Create the database engine
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
