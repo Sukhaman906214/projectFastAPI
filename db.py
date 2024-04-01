4dcf2228303e167d53efe31623aa6319178b1609
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-04-01 09:54:23 +0500

    db.py

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite:///./lg.db"
 # Create the database engine
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionalLocal = sessionmaker(autocommit= False ,autoflush=Falase, bind=engine)
