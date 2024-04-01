49900e06152f23bf6a00c94faf48e685fa8e0b31
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-04-01 09:57:46 +0500

    db.py

--- db.py
+++ db.py
@@ -4,4 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 DATABASE_URL = "sqlite:///./lg.db"
 # Create the database engine
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+#Sessional class to interact withh the database
 SessionalLocal = sessionmaker(autocommit= False ,autoflush=False, bind=engine)
+#Declarative Base for ORM 
+Base  = declarative_base()
