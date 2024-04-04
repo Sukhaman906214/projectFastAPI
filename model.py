e6abb18d832923b2aee08a54858ba162e3f4f927
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-04-04 14:03:55 +0500

    model.py

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, DateTime, Integer, String
 from db import Base
 from sqlalchemy import func
-from sqlalchemy import func
+
 class enrty(Base):
     __tablename__ = "sinup"
 
