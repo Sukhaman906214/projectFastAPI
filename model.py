f5bb079c2e6e40d23c13d42369f2f28dfbb4a738
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-04-04 14:03:18 +0500

    model.py

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, DateTime, Integer, String
 from db import Base
 from sqlalchemy import func
+
 class enrty(Base):
     __tablename__ = "sinup"
 
@@ -8,3 +9,11 @@ class enrty(Base):
     email = Column(String, unique=True, index=True)
     password = Column(String)
     full_name = Column(String, index=True)
+class JobSubmission():
+    __tablename = "job_submission"
+       id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, index=True)
+    job_title = Column(String, index=True)
+    description = Column(String)
+    deadline = Column(DateTime)
+    priority = Column(String)
