94c123a1e4abc78d9347b9d2822f576ed8984b23
Sukhaman906214 <164368964+Sukhaman906214@users.noreply.github.com>
2024-04-05 15:36:10 +0500

    model.py

--- model.py
+++ model.py
@@ -11,9 +11,10 @@ class enrty(Base):
     full_name = Column(String, index=True)
 class JobSubmission():
     __tablename = "job_submission"
-       id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, index=True)
     job_title = Column(String, index=True)
     description = Column(String)
     deadline = Column(DateTime)
     priority = Column(String)
+    submission_time = Column(DateTime, server_default=func.now())
